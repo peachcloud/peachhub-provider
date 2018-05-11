11d944397c99a1d2ff0a265847ea6eb384a236d5
Michael Williams <michael.williams@enspiral.com>
2018-05-11 17:43:51 +1200

    oh, thanks new error log

--- worker/index.js
+++ worker/index.js
@@ -109,7 +109,6 @@ async function createWorker ({ connection, log, queue: queueName }) {
   })
 
   await worker.connect()
-  await worker.workerCleanup() // optional: cleanup any previous improperly shutdown workers on this host
   worker.start()
 
   return worker
